757d47d6811b9e5b15d7873a9ad63a422e70451a
Shubert <alonzoshubert6991@gmail.com>
2020-12-21 21:43:00 -0600

    Home done, working on projects

--- new/src/components/Home.tsx
+++ new/src/components/Home.tsx
@@ -4,13 +4,15 @@ import "./Home.css";
 import ParticlesBg from "particles-bg";
 
 import Card from "react-bootstrap/Card";
-import logo from "../images/logo.png";
+import Image from "react-bootstrap/Image";
+import pic from "../images/me.jpg";
+
 const Home = () => {
   return (
-    <div>
+    <div className="d-flex justify-content-center divhome">
       <ParticlesBg type="cobweb" bg={true} color="#81ACD8" />
-      <Card>
-        <Card.Img variant="top" src={logo} />
+      <Card className="homecard">
+        <Card.Img src={pic}   />
         <Card.Body>
           <Card.Title>Shubert Alonzo</Card.Title>
           <Card.Text>Web Developer</Card.Text>
