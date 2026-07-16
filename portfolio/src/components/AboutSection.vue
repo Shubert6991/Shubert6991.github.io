dc1b98ef17d37623447c7e5e877d5a9638346001
Shubert Alonzo <alonzoshubert6991@gmail.com>
2026-07-15 20:05:50 -0600

    add tailwind

--- portfolio/src/components/AboutSection.vue
+++ portfolio/src/components/AboutSection.vue
@@ -3,5 +3,8 @@
 <template>
   <div>
     <h2>About</h2>
+    <p class="font-mono text-terminal-500 text-lg mb-2">
+      > hello_world <span class="animate-pulse">_</span>
+    </p>
   </div>
 </template>
