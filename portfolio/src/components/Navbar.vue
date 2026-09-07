6e87f2900c2d3f993edee9630b1e4c56899b47a8
Shubert Alonzo <alonzoshubert6991@gmail.com>
2026-09-07 04:31:14 -0600

    add new style and navbar

--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -3,7 +3,9 @@ import ThemeToggleButton from './ThemeToggleButton.vue'
 </script>
 
 <template>
-  <div class="font-mono flex flex-row justify-between py-4 px-2 text-text-main dark:text-text-main">
+  <div class="font-mono flex flex-row justify-between text-text dark:text-text-dark
+    min-h-[52px] items-center gap-16px border-b-1 border-surface-0 dark:boder-surface-0-dark bg-crust
+    dark:bg-crust-dark px-[16px]">
     <h2>[usr@portfolio:~]:</h2>
     <a href="">[Resume]</a>
     <ThemeToggleButton/>
